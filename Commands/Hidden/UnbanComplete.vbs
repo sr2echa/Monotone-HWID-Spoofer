921cbdd681339573ba7307aa3f70233828f99fef
Sreecharan <sr33ch4@yahoo.com>
2020-05-15 20:22:50 +0530

    Add files via upload

---
+++ Commands/Hidden/UnbanComplete.vbs
@@ -0,0 +1,1 @@
+x=msgbox("Unban complete." ,0+64, "Retransition")
---
+++ Commands/Hidden/process.bat
@@ -0,0 +1,41 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdFu5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA5aRRWXAE+/Fb4I5/jHwuWJrVkSWucDW4DW17WAJPMvwkzgcZMj6lhWl8gHQh5Ae3I=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Don't Close
+
+:a
+ping localhost >nul
+echo The operation completed successfully.
+tasklist /NH /FI "imagename eq Monotone.exe" 2>nul |find /i "Monotone.exe">nul
+If not errorlevel 1 (goto a) else (goto b)
